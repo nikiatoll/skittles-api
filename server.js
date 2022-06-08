2ffd26941f3d201432b67af2de0d7e5b19771a43
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-06-08 09:20:07 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+PORT = 8000
+
+app.use(cors())
+
+const flavors={
+    'original': {
+        'red': "Strawberry",
+        "yellow" : "Lemon",
+        'orange' : 'Orange',
+        'purple' : 'Grape',
+        'green' : 'Lime'
+    },
+    'wild Berry': {
+        'green': 'Mellon Berry',
+        'blue': 'Raspberry',
+        'red': 'Wild Cherry',
+        'purple': 'Berry Punch',
+        'pink': 'Strawberry'
+    },
+    'brightside': {
+        'orange': 'Tangerine',
+        'pink': 'Pink Lemonade',
+        'yellow': 'Kiwi Banana',
+        'green': 'Watermelon',
+        'blue': 'Paradise Punch'
+    },
+    'smoothie': {
+        'purple': 'Blueberry Smoothie',
+        'red': 'Raspberry Smoothie',
+        'yellow': 'Peach Guava Smoothie',
+        'pink': 'Strawberry Banana Smoothie',
+        'peach': 'Mango Smoothie',
+    },
+    'tropical': {
+        'orange': 'Mango Tangelo',
+        'yellow': 'Banana Berry',
+        'blue': 'Pinapple Passionfruit',
+        'pink': 'Strawberry Starfruit',
+        'green': 'Kiwi Lime',
+    },
+    'unknown': {
+        'unknown':'unknown',
+    }
+}
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
+app.get('/api/:name',(req, res)=>{
+    const skittlesFlavor = req.params.name.toLowerCase()
+    if (flavors[skittlesFlavor]){
+        res.json(flavors[skittlesFlavor])
+    } else {
+        res.json(flavors['unknown'])
+    }
+})
+
+app.listen(PORT , ()=>{
+    console.log(`The server is running on port ${PORT}! Betta go catch it!`)
+})
