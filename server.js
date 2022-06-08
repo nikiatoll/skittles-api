582af11135de7517d999791902aec5fb6b322e3d
Maverick Fernandez <maverick.fernandez8@gmail.com>
2022-06-08 09:37:09 -0400

    heroku changes

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get('/api/:name',(req, res)=>{
     }
 })
 
-app.listen(PORT , ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}! Betta go catch it!`)
 })
